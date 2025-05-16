7e3da5e17696e8afdb87471216254465f2b0e12c
shew33 <shew33@icloud.com>
2025-05-16 16:05:50 +0300

    Финальные изменения согласно рекомендациям

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,52 @@
 package jm.task.core.jdbc;
 
-import java.util.List;
-
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl();
+        Properties properties = new Properties();
+
+        // Загружаем application.properties
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                logger.error("application.properties не найден");
+                return;
+            }
+            properties.load(input);
+        } catch (Exception e) {
+            logger.error("Ошибка при загрузке application.properties", e);
+            return;
+        }
+
+        // Определяем реализацию DAO
+        String realization = properties.getProperty("realization");
+        UserDao userDao;
+        if ("jdbc".equalsIgnoreCase(realization)) {
+            userDao = new UserDaoJDBCImpl();
+            logger.info("Используется реализация: JDBC");
+        } else if ("hibernate".equalsIgnoreCase(realization)) {
+            userDao = new UserDaoHibernateImpl();
+            logger.info("Используется реализация: Hibernate");
+        } else {
+            logger.error("Недопустимое значение realization в application.properties: {}", realization);
+            return;
+        }
+
+        // Инжектим реализацию в сервис
+        UserService userService = new UserServiceImpl(userDao);
 
         userService.createUsersTable();
 
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,34 +4,76 @@ import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 
 public class UserServiceImpl implements UserService {
-    private final UserDao userDao = new UserDaoHibernateImpl();
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+    private final UserDao userDao;
 
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public UserServiceImpl() {
+        this.userDao = loadUserDaoFromProperties();
+    }
+
+    private UserDao loadUserDaoFromProperties() {
+        Properties properties = new Properties();
+        try (InputStream input = UserServiceImpl.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                logger.error("Файл application.properties не найден");
+                throw new IllegalStateException("Файл application.properties не найден");
+            }
+            properties.load(input);
+            String realization = properties.getProperty("realization");
+
+            if ("jdbc".equalsIgnoreCase(realization)) {
+                return new UserDaoJDBCImpl();
+            } else if ("hibernate".equalsIgnoreCase(realization)) {
+                return new UserDaoHibernateImpl();
+            } else {
+                throw new IllegalArgumentException("Некорректное значение realization: " + realization);
+            }
+
+        } catch (Exception e) {
+            logger.error("Ошибка при инициализации UserDao", e);
+            throw new IllegalStateException("Ошибка при инициализации UserDao", e);
+        }
+    }
+
+    @Override
     public void createUsersTable() {
         userDao.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
         userDao.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         userDao.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
         userDao.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         userDao.cleanUsersTable();
     }
 }
-
