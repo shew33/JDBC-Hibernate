d3a53a2943d34fba9c0c6b52131f60451aa819d7
shew33 <shew33@icloud.com>
2025-05-16 14:53:00 +0300

    Изменения согласно рекомендациям

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -28,10 +28,6 @@ public class User {
         this.age = age;
     }
 
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,53 +1,71 @@
 package jm.task.core.jdbc.util;
 
-import org.hibernate.cfg.Configuration;
+import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-
-import jm.task.core.jdbc.model.User;
-import org.hibernate.cfg.Environment;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class Util {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "3748";
+    private static final Logger logger = LoggerFactory.getLogger(Util.class);
+    private static final Properties properties = new Properties();
 
-    // JBDC часть
-    public static Connection getConnection() {
-        try {
-            Class.forName("org.postgresql.Driver"); //обязательно добавить
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("PostgresSQL JDBC Driver не найден", e);
-        } catch (SQLException e) {
-            throw new RuntimeException("ошибка подключения к бд", e);
+    static {
+        try (InputStream input = Util.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                logger.error("файл .properties не найден");
+                throw new RuntimeException("не найден файл .properties");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            logger.error("ошибка при загрузке .properties", e);
+            throw new RuntimeException("ошибка при загрузке .properties", e);
         }
     }
 
-    // Hibernate часть
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    properties.getProperty("db.url"),
+                    properties.getProperty("db.user"),
+                    properties.getProperty("db.password")
+            );
+        } catch (SQLException e) {
+            logger.error("ошибка подключения к БД", e);
+            throw new RuntimeException("ошибка подключения к БД", e);
+        }
+    }
+
     private static final SessionFactory sessionFactory;
 
     static {
         try {
             Configuration configuration = new Configuration();
             Properties settings = new Properties();
-            settings.put(Environment.DRIVER, "org.postgresql.Driver");
-            settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/postgres");
-            settings.put(Environment.USER, "postgres");
-            settings.put(Environment.PASS, "3748");
-            settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
-            settings.put(Environment.SHOW_SQL, "true");
-            settings.put(Environment.HBM2DDL_AUTO, "update"); //временно для авто-создания
+
+            settings.put(Environment.DRIVER, properties.getProperty("db.driver"));
+            settings.put(Environment.URL, properties.getProperty("db.url"));
+            settings.put(Environment.USER, properties.getProperty("db.user"));
+            settings.put(Environment.PASS, properties.getProperty("db.password"));
+            settings.put(Environment.DIALECT, properties.getProperty("hibernate.dialect"));
+            settings.put(Environment.SHOW_SQL, properties.getProperty("hibernate.show_sql"));
+            settings.put(Environment.HBM2DDL_AUTO, properties.getProperty("hibernate.hbm2ddl_auto"));
 
             configuration.setProperties(settings);
             configuration.addAnnotatedClass(User.class);
 
             sessionFactory = configuration.buildSessionFactory();
         } catch (Exception e) {
-            throw new RuntimeException("Ошибка создания SessionFactory", e);
+            logger.error("ошибка создания SessionFactory", e);
+            throw new RuntimeException("ошибка создания SessionFactory", e);
         }
     }
 
@@ -55,3 +73,4 @@ public class Util {
         return sessionFactory;
     }
 }
+
